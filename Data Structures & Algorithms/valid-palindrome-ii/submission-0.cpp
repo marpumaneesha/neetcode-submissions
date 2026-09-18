856702e9ce1d87c1aa5c303e78e29e9a194bf8ad
marpumaneesha <marpumaneesha190406@gmail.com>
2026-09-18 16:16:40 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int l=0,r=s.size()-1;
+        while(l<r){
+            if(s[l]!=s[r]){
+                return ispal(s.substr(0,l)+s.substr(l+1)) || ispal(s.substr(0,r)+s.substr(r+1));
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+    bool ispal(string s){
+       int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (s[l] != s[r]) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true; 
+    }
+};

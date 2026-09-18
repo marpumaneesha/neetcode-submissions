574f6a1a13b6b095baae8a2b9a458f1c8fee82f6
marpumaneesha <marpumaneesha190406@gmail.com>
2026-09-18 11:50:49 +0530

    Add: count-paths - submission-5

---
+++ Data Structures & Algorithms/count-paths/submission-5.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    /*int f(int &m,int &n,int i,int j,vector<vector<int>>&dp){
+        if(i==m-1 && j==n-1) return 1;
+        if(i>=m || j>=n) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int down=f(m,n,i+1,j,dp);
+        int right=f(m,n,i,j+1,dp);
+        return dp[i][j]=down+right;
+    }*/
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        dp[m-1][n-1]=1;
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                dp[i][j]+=dp[i+1][j]+dp[i][j+1];
+            }
+        }
+        return dp[0][0];
+    }
+};

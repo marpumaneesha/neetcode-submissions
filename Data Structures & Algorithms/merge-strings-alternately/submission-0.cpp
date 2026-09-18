9aa1d0ab533d8820dd78f09d2b7f6c4ed1bc8e53
marpumaneesha <marpumaneesha190406@gmail.com>
2026-09-18 16:24:48 +0530

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int n1=word1.size();
+        int n2=word2.size();
+        string ans;
+        for(int i=0;i<n1||i<n2;i++){
+            if(i<n1) ans+=word1[i];
+            if(i<n2) ans+=word2[i];
+        }return ans;
+    }
+};

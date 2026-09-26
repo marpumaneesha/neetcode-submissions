9e6755c794afbc58b778bd20f11c9fc4dc4fbe89
marpumaneesha <marpumaneesha190406@gmail.com>
2026-09-26 12:40:52 +0530

    Add: transpose-matrix - submission-0

---
+++ Data Structures & Algorithms/transpose-matrix/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<vector<int>> trans(m,vector<int>(n));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                trans[i][j]=matrix[j][i];
+            }
+        }return trans;
+    }
+};

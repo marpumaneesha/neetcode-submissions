c8e5518d0779dbcac10470e700d83fa62b83c976
marpumaneesha <marpumaneesha190406@gmail.com>
2026-09-26 12:01:36 +0530

    Add: find-the-town-judge - submission-0

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int>t(n+1,0);
+        for(auto &it:trust){
+            t[it[0]]--;
+            t[it[1]]++;
+        }
+        for(int i=1;i<=n;i++){
+            if(t[i]==n-1) return i;
+
+        }return -1;
+    }
+};
